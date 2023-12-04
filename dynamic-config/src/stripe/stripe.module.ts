3313c439e806fabe0bd0853a9be96d0b71abd54d
Emmanuel odii <emmanuelodii80@gmail.com>
2023-12-04 11:27:05 +0100

    chore: add env vars and stripe module

---
+++ dynamic-config/src/stripe/stripe.module.ts
@@ -0,0 +1,28 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StripeController } from './stripe.controller';
+import { StripeService } from './stripe.service';
+
+@Module({})
+export class StripeModule {
+  /**
+   * We have stripe's api-key as an env var, so we use factory functions
+   * Also using the forRootAsync convention because it loads the api-key asynchronously
+   */
+  static forRootAsync(): DynamicModule {
+    return {
+      module: StripeModule,
+      controllers: [StripeController],
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        StripeService,
+        {
+          provide: 'STRIPE_API_KEY',
+          useFactory: async (configService: ConfigService) =>
+            configService.get('STRIPE_API_KEY'),
+          inject: [ConfigService],
+        },
+      ],
+    };
+  }
+}
